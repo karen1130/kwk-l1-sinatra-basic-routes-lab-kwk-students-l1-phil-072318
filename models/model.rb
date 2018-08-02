808827af761e8bdfefb33e4beab029139b49ff5c
Karen Kraft <kraftkj@comcast.net>
2018-08-02 16:48:25 +0000

    automatically backed up by learn

--- models/model.rb
+++ models/model.rb
@@ -8,7 +8,7 @@ def equality_quiz_question1(answer)
   end 
 end 
 
-def guncontrol_quiz_question2(answer)
+def equality_quiz_question2(answer)
   if answer == "answer2"
     @@results.push("AWesome")
   else 
@@ -16,7 +16,7 @@ def guncontrol_quiz_question2(answer)
   end 
 end 
 
-def guncontrol_quiz_question3(answer)
+def equality_quiz_question3(answer)
   if answer == "answer2"
     @@results.push("AWesome")
   else 
